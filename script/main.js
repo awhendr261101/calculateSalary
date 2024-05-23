7f8e106b493a68715212d54bc169899229d8a909
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-05-23 17:29:43 +0200

    lets give it a rest

--- script/main.js
+++ script/main.js
@@ -1,17 +1,64 @@
+let fname = document.querySelector(".fname")
+let lname = document.querySelector(".lname")
+let dob = document.querySelector(".dob")
+let textFields = document.querySelectorAll("[textField]")
+let submit = document.querySelector(".submit")
+let error = document.querySelectorAll(".error")
 
-let work = document.querySelector(".work")
-let hidden = document.querySelector(".inContainer")
-console.log(work);
+// Vars for the smaller form 
+let calculate = document.querySelector(".calculate")
+let hourlyR = document.querySelector(".hourlyRate")
+let hours = document.querySelector(".hours")
+let calfields = document.querySelector("[calfield]")
 
-work.addEventListener("click", ()=>{
-    if(work.value == "Yes"){
-        hidden.style.display = "block"
+
+
+submit.addEventListener("click", () =>{
+    textFields.forEach(validator)
+})
+
+function validator (value, index) {
+    if(value.value == ""){
+        error[index].innerText = "Please enter this field!!"
+    } else if(notValidText(value.value)){
+        error[index].innerText = "My brodda why would you add numbers in this, wetin be diss, wetin be diss!!"
     }else{
-        hidden.style.display = "none"
+        error[index].innerText = ""
     }
+}
+
+calculate.addEventListener("click", () => {
+
 })
 
 
 
 
+function notValidText(value){
+    let numbers = "0123456789"
+        for (const key in numbers) {
+            if(value.includes(numbers[key])){
+                return true
+            }
+        }
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let names = ["John", "Sarah", "Michael", "Emily", "David"];
+
+// names.forEach((name, index) => {
+//   console.log(`${index }. ${name}`);
+// });
